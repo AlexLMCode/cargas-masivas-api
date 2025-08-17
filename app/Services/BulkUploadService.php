613a62b1ec6f872eb2cbe1db33487c4cf6500954
Alex Lujan <alexlujanm@outlook.com>
2025-08-16 23:55:27 -0600

    code clean

--- app/Services/BulkUploadService.php
+++ app/Services/BulkUploadService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
 
 class BulkUploadService
 {
@@ -17,13 +18,12 @@ class BulkUploadService
     {
         try {
             $store = $request->storeAs('mysql_uploads', 'personas.csv');
+            $localPath = storage_path("app/{$store}");
             $fullPath = '/var/www/html/storage/mysql_uploads/personas.csv';
 
             $pdo = DB::connection()->getPdo();
-
-            $pdo->exec("TRUNCATE TABLE tmp_personas");
-
-            $pdo->exec("
+            $pdo->beginTransaction();
+            $query = "
                 LOAD DATA LOCAL INFILE '" . addslashes($fullPath) . "'
                 INTO TABLE tmp_personas
                 FIELDS TERMINATED BY ',' 
@@ -31,9 +31,16 @@ class BulkUploadService
                 LINES TERMINATED BY '\\n'
                 IGNORE 1 LINES
                 (nombre, paterno, materno, telefono, calle, numero_exterior, numero_interior, colonia, cp)
-            ");
+            ";
+
+            $pdo->exec($query);
+            $pdo->commit();
+
+            File::delete($localPath);
 
             $pdo->exec("CALL sp_cargar_personas()");
+
+            $pdo->exec("TRUNCATE TABLE tmp_personas");
 
             return true;
         } catch (\Throwable $th) {
